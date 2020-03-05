0b48c244ae59b9c077b13565a383b5435a8d676b
joselayana <jooselayan@gmail.com>
2020-03-04 23:15:16 -0300

    register listo mas map de carrito avanzado

--- front/src/components/Cart.jsx
+++ front/src/components/Cart.jsx
@@ -1,30 +1,54 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 
-export default ({user}) => {
-
-    return (
-        
-        <div>
-            <label></label>
-        {user.username ?(
-            <h2>{`Carrito de ${user.username}`}</h2>
-            
-            ):( <h2>Carrito</h2>)}
-            <hr></hr>
-        <div className = "container">
-            <div class="card">
-                <h5 class="card-header">Nombre del producto del carrito</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Bodega</h5>
-                    <p class="card-text">Precio del producto</p>
-                    <button type="button" class="btn btn-outline-danger">Remover del Carrito</button>
-                </div>
+export default ({user, cart}) => {
+    if(user.username){
+        return(
+            <div>
+                <label></label>
+                <h2>{`Carrito de ${user.username}`}</h2>
+                <hr></hr>
+                <div className = "container">
+                    {cart.map(item => {
+                    return (
+                        <figure key={item.id}>
+                            <Link to={`/wines/${item.productoId}`}>
+                                <div class="card">
+                                    <h5 class="card-header">Nombre del producto del carrito</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Bodega</h5>
+                                        <p class="card-text">Precio del producto</p>
+                                        <button type="button" class="btn btn-outline-danger">Remover del Carrito</button>
+                                    </div>
+                                </div>
+                            </Link>
+                        </figure>
+                    )
+                    })}   
+                </div >
+                <label></label>
             </div>
-        </div >
-        <label></label>
-        </div>
-
-    )
+        )
+    }else{
+        return (
+            <div>
+                <label></label>
+                <h2>Carrito</h2>
+                <hr></hr>
+                    <div className = "container">
+                        <div class="card">
+                            <h5 class="card-header">Nombre del producto del carrito</h5>
+                            <div class="card-body">
+                                <h5 class="card-title">Bodega</h5>
+                                <p class="card-text">Precio del producto</p>
+                                <button type="button" class="btn btn-outline-danger">Remover del Carrito</button>
+                            </div>
+                        </div>
+                    </div >
+                <label></label>
+            </div>
+    
+        )
+    }
 }
